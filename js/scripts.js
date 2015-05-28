1af4514ca7c0e4f4ebb4647b1096e82254b0511a
Courtney Phillips & Chris Sefkin <student@epicodus.com>
2015-05-28 14:04:26 -0700

    roman numerals suck

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,98 @@
-var add = function(num) {
- var numSum = []
+var roman = function(number) {
+  var answer = []
+  var num = number;
 
-  var rValues = [1000,500,100,50,10,5,1]
+  while (num >= 1000) {
+    answer.push("M")
+    num = (num - 1000);
+  }
 
-  var newNum = num
+while (num >= 500) {
+  answer.push("D")
+  num = (num - 500);
+}
 
-  rValues.forEach(function(value) {
-    var divide = (newNum / value);
-    if (divide >= 1) {
-      numSum.push(value)
-      newNum = (newNum - value);
-    }
-    if (newNum > 0) {
-      divide = (newNum / 1)
-      numSum.push(1)
-      newNum = (newNum - 1);
-    }
-  })
-    return numSum;
-  };
+while (num >= 100) {
+  answer.push("C")
+  num = (num - 100);
+}
+
+while (num >= 50) {
+  answer.push("L")
+  num = (num - 50);
+}
+
+while (num >= 40) {
+  answer.push("L")
+}
+
+while (num >= 10) {
+  answer.push("X")
+  num = (num - 10);
+}
+
+while (num >= 5) {
+  answer.push("V")
+  num = (num - 5);
+}
+
+while (num >= 1) {
+  answer.push("I")
+  num = (num - 1);
+}
+
+return answer;
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var add = function(num) {
+//  var numSum = []
+//
+//   var rValues = [1000,500,100,50,10,5,1]
+//
+//   var newNum = num
+//
+//   rValues.for(var index = 1; ) {
+//     var divide = (newNum / value);
+//     if (divide >= 1) {
+//       numSum.push(value)
+//       newNum = (newNum - value);
+//     }
+//     if (newNum > 0) {
+//       divide = (newNum / 1)
+//       numSum.unshift(1)
+//       newNum = (newNum - 1);
+//     }
+//   })
+//     return numSum;
+//
+//     numSum.forEach(function(num) {
+//
+//     })
+//   };
--- spec/specs.js
+++ spec/specs.js
@@ -1,11 +1,11 @@
-describe('add', function(){
+describe('roman', function(){
 
   // it("will return the value of a single roman numeral", function() {
   //   expect(add(5)).to.equal("V")
   // });
 
   it("will total the roman numerals", function() {
-    expect(add(102)).to.equal("LXVI")
+    expect(roman(2503)).to.equal("LXVI")
   });
 
 
